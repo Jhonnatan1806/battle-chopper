45d901ef0085aa37b13be58f3bad8fe4057378c2
Jhonnatan Espinoza Rojas <hytzy18@gmail.com>
2023-10-04 02:34:14 -0500

    update

---
+++ src/main/java/com/project/simplegame/client/Client.java
@@ -0,0 +1,27 @@
+package com.project.simplegame.client;
+
+import javax.swing.*;
+import java.util.logging.Logger;
+
+
+public class Client {
+
+    public static void main(String args[]) {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(ClientView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ClientView().setVisible(true);
+            }
+        });
+    }
+}
